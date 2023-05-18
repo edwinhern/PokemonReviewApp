408fab38c7c1be45998e695086079f5d3ddf7412
Edwin Hernandez <Edwinhern.16@gmail.com>
2023-05-18 12:38:03 -0500

    initial commit

---
+++ PokemonReviewApp/Models/Pokemon.cs
@@ -0,0 +1,11 @@
+﻿namespace PokemonReviewApp.Models
+{
+	// Model: Used to represent the data inside the database (MySQL)
+	public class Pokemon
+	{
+		public int Id { get; set; }
+		public string? Name { get; set; }
+		public DateTime BirthDate { get; set; }
+	}
+}
+
